930996d7278b5203f5c7de00712ed5832a5f76db
Davor Hrg <hrgdavor@gmail.com>
2025-09-09 13:06:52 +0200

    Update HipsterContext.java

--- ioc/src/main/java/hr/hrg/hipster/ioc/HipsterContext.java
+++ ioc/src/main/java/hr/hrg/hipster/ioc/HipsterContext.java
@@ -5,7 +5,20 @@ import java.lang.annotation.RetentionPolicy;
 
 @Retention(RetentionPolicy.RUNTIME)
 public  @interface  HipsterContext {
-    /** If an implementation already exists, disable auto-build */
-    Class<?> impl() default Void.class;
     Class<?>[] dependencies() default {};
+    Class<?> factory() default Void.class;
+
+    /// Enforce stricter rules
+    /// - no depndency on own context allowed
+    /// @return
+    boolean strict() default false;
+
+    /// Clarifies that this bean is not a context intended for auto-generation, but allow expanding publicly available
+    /// properties or zero parameter methods as dependencies. Allows for stricter enforcement during compilation
+    /// and shows the difference in the intended use.
+    boolean expandOnly() default false;
+
+    /// If an implementation already exists, disable auto-build and give reference to it for documentation purposes
+    Class<?> impl() default Void.class;
+
 }
